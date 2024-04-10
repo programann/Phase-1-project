4dd96ee14602d94c61210fefe2b4cf0f54a8c04f
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-04-10 21:15:17 +0300

    phase-1 project

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    let getButton = document.querySelector('button')
     let url = 'http://api.nbp.pl/api/cenyzlota/last/30/?format=json'
 
-    let div = document.querySelector('#apicontent')
-        
-    let p = document.createElement('p')
-    div.append(p)
-    p.textContent = ''
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>
+        data.forEach(data=>addDate(data.data)));
+
+    getButton.addEventListener('click',(e)=>{
+        e.preventDefault()
+    })
+
+    function addDate(value){
+        let div = document.querySelector('apicontent')
+        let p = document.createElement('p')
+        p.textContent = value
+        div.append(p)
+    }
 
 })
