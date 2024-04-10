4a3c9739ae1babe5bff078067262710d45c921b4
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-04-10 17:11:42 +0300

    phase-1 project

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded",()=>{
     let url = 'http://api.nbp.pl/api/cenyzlota/last/30/?format=json'
 
     let div = document.querySelector('#apicontent')
+        
     let p = document.createElement('p')
     div.append(p)
-
-    p.innerText = ''
+    p.textContent = ''
 
 })
